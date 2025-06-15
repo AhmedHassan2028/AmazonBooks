58ec6f1f13615291657c94cda5276e3c8bbfe215
Ahmed Hassan <ahfa871@gmail.com>
2025-06-14 22:33:35 -0400

    Initial commit

---
+++ src/books.js
@@ -0,0 +1,29 @@
+import img1 from './images/book-1.jpg'
+import img2 from './images/book-2.jpg'
+import img3 from './images/book-3.jpg'
+
+export const books = [
+  {
+  author: 'Jordan Moore',
+  title: 'Smarts',
+  img:img1,
+  id:1,
+},
+
+ {
+  author: 'Ahmed Hassan',
+  title: 'My Book',
+  img:img2,
+  id: 2,
+},
+
+  {
+  author: 'Joker man',
+  title: 'Dad jokes',
+  img:img3,
+  id: 3,
+}
+
+
+]
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import { books } from './books';
+import Book from './Book';
+
+function BookList() {
+  return (
+    <>
+      <h1>amazon best sellers</h1>
+      <section className='booklist'>
+        {books.map((book) => {
+          return <Book {...book} key={book.id} />;
+        })}
+      </section>
+    </>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<BookList />);
+
+//{...Book} is like Book={book}
+//Map Creates new array from calling a function for every array element
+//Cannot render object directly in jsx, ex: cant call an array of objects {books} within component
+//You can replace proprs argument with {img,title,author} and remove that extra line of code
+//For children prop: {img,title,author,children} then render it inside the component as {children}
+//Children will include the unique items for a specific book component within its tags
+
+
+//Events: element, event function (camelCase), common: onClick, onSubmit, onChange
+//View filter and find js viideo
+//later down there is onClick button with {getBook(id)}, problem is function gets ran immediatly due to parantheses, so a fix could be is creating... 
+//in book component getSingleBook which invokes the function and then on click in calls getSingleBook
+
+
+//type submit to submit the form
+//Data can only pass props down, ie booklist to book (parent to child)
